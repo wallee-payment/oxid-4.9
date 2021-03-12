9c3fd6f050dc8f6878e28bf1aa38132dd180d1f5
Simon Schurter <simon.schurter@wallee.com>
2021-03-12 17:05:22 +0100

    Release 1.0.26

--- wle/Wallee/metadata.php
+++ wle/Wallee/metadata.php
@@ -31,7 +31,7 @@ $aModule = array(
         'en' => 'WLE Wallee Module'
     ),
     'thumbnail' => 'out/pictures/picture.png',
-    'version' => '1.0.25',
+    'version' => '1.0.26',
     'author' => 'customweb GmbH',
     'url' => 'https://www.customweb.com',
     'email' => 'info@customweb.com',
