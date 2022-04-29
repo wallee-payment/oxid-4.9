e9f56a8106e9a849b62392278811674b54d53fa7
Edgaras <edgaras.tkacenka@wallee.com>
2022-04-29 13:36:27 +0300

    Release 1.0.33

--- wle/Wallee/metadata.php
+++ wle/Wallee/metadata.php
@@ -31,7 +31,7 @@ $aModule = array(
         'en' => 'WLE Wallee Module'
     ),
     'thumbnail' => 'out/pictures/picture.png',
-    'version' => '1.0.32',
+    'version' => '1.0.33',
     'author' => 'customweb GmbH',
     'url' => 'https://www.customweb.com',
     'email' => 'info@customweb.com',
